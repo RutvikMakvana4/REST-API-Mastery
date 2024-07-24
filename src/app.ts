5fd515ab760b02bfd030d8cb84f5051cd9e613bf
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2024-07-24 22:30:25 +0530

    feat: user router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import globalErrorHandler from "./middleware/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to home route" });
 });
+
+app.use("/api/users", userRouter);
 
 // global error handler
 app.use(globalErrorHandler);
