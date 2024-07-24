9931a0a3e5a8cf09cdd07338fcd46dd7cb6536c8
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2024-07-24 22:42:59 +0530

    feat: add register user validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,14 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+
   res.json({ message: "User created" });
 };
 
